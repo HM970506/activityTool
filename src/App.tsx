3ffd0c876abb58fdae21fb7698da2a20b58d3c43
mimi <62527898+HM970506@users.noreply.github.com>
2023-02-27 15:39:24 +0900

    초기 라이브러리 설치 및 페이지 라우팅 작업

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Reset } from "styled-reset";
+import TemplateActivity from "./pages/templateActivity";
+import TemplatesIndex from "./pages/templateList";
+
+function App() {
+  return (
+    <>
+      <Reset />
+      <Routes>
+        <Route path="/" element={<TemplatesIndex />} />
+        <Route path="/:templateId" element={<TemplateActivity />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
