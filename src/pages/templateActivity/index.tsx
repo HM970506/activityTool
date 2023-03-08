74291378c9bda936cfebb7482de35b60ea254b53
mimi <62527898+HM970506@users.noreply.github.com>
2023-03-08 13:25:36 +0900

    컬러 추출기 적용

--- src/pages/templateActivity/index.tsx
+++ src/pages/templateActivity/index.tsx
@@ -1,13 +1,37 @@
 import { useParams } from "react-router-dom";
-import { Background, Template } from "./style";
-import { ColorExtractor } from "react-color-extractor";
+import { ColorBox, ColorChip, Template } from "./style";
+import { extractColors } from "extract-colors";
+import { useEffect, useState } from "react";
 
 export default function TemplateActivity() {
   const { templateId } = useParams();
+  const url = `./test${templateId}.PNG`;
+  const [colors, setColors] = useState<any[]>([]);
+  const [backgroundColor, setBackgroundColor] = useState<string>("white");
+
+  const getColor = async () => {
+    const test1 = await extractColors(url);
+    if (colors != test1) setColors(test1);
+  };
+
+  useEffect(() => {
+    getColor();
+  }, []);
 
   return (
-    <ColorExtractor getColors={this.getColors}>
-      <Template image={templateId!} />
-    </ColorExtractor>
+    <>
+      <Template url={url} backgroundcolor={backgroundColor} />
+      <ColorBox>
+        {colors.map((value, key) => (
+          <ColorChip
+            key={key}
+            backgroundcolor={value.hex}
+            onClick={() => {
+              setBackgroundColor(value.hex);
+            }}
+          />
+        ))}
+      </ColorBox>
+    </>
   );
 }
--- src/pages/templateActivity/style.ts
+++ src/pages/templateActivity/style.ts
@@ -1,11 +1,33 @@
 import styled from "styled-components";
 
-export const Template = styled.main<{ image: string }>`
-  background-image: ${(props) => `url(./test${props.image}.PNG)`};
+export const Template = styled.main<{ url: string; backgroundcolor: string }>`
+  background-image: ${(props) => `url(${props.url})`};
   background-repeat: no-repeat;
   background-size: contain;
   background-position: center;
-  background-color: black;
-  width: 100vw;
+  background-color: ${(props) => props.backgroundcolor};
+  width: 100%;
   height: 100vh;
+  overflow: hidden;
+  position: fixed;
+`;
+
+export const ColorBox = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100px;
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  overflow: auto;
+  display: flex;
+  align-item: center;
+`;
+
+export const ColorChip = styled.div<{ backgroundcolor: string }>`
+  width: 30px;
+  height: 30px;
+  margin: 10px;
+  border-radius: 100%;
+  background-color: ${(props) => props.backgroundcolor};
 `;
