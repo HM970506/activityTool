1e7a387acc0f6a788dc35feb79a5ef58aecfe62b
mimi <62527898+HM970506@users.noreply.github.com>
2023-03-21 14:48:35 +0900

    토글 주석 추가

--- src/components/newactivitytool/topButtons/toggleButton/toggleButton.tsx
+++ src/components/newactivitytool/topButtons/toggleButton/toggleButton.tsx
@@ -10,18 +10,22 @@ export default function ToggleButton() {
   const canvas = useSelector((state: any) => state.nodeReducer.canvas);
   const category = useSelector((state: any) => state.categoryReducer.category);
 
-  //그림그리기 상태는 다음과 같을 때 바뀜.
-  //1.토글 버튼을 클릭했을 때
+  //isCheck--영향 단방향-->canvas
+  //isCheck상태는 변할 때 그림그리기 상태를 바꿈.
+  //그림그리기 상태는 변할 때 isCheck상태를 바꾸지 않음.
+
+  //isCheck가 true인데 그림그리기 상태가 false일 수 있음. ex)그림도구 중 스탬프를 사용할 때..
+  //isCheck가 false인데 그림그리기 상태가 true일 수 없음.
   useEffect(() => {
     if (canvas) {
       canvas.discardActiveObject().renderAll();
       canvas.isDrawingMode = isCheck;
       dispatch(drawActions.setDraw(isCheck));
-      console.log("toggle: ", isCheck, "캔버스 :", canvas.isDrawingMode);
     }
   }, [isCheck]);
 
-  //2.카테고리가 바뀌었을 때
+  //ischeck상태는 다음과 같이 바뀜.
+  //1.카테고리가 바뀌었을 때
   useEffect(() => {
     if (category != "DRAWTOOLS") setIsCheck(false);
   }, [category]);
@@ -31,7 +35,10 @@ export default function ToggleButton() {
       <Toggle
         type="checkbox"
         checked={isCheck}
-        onChange={(e) => setIsCheck(e.target.checked)}
+        onChange={
+          //2.토글 버튼을 클릭했을 때
+          (e) => setIsCheck(e.target.checked)
+        }
       />
       <Slider></Slider>
     </Label>
