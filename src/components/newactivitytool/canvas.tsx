c04df1219451eec23825730d6544aeb77f77e4a3
mimi <62527898+HM970506@users.noreply.github.com>
2023-03-22 13:19:43 +0900

    모듈에러 관련 추가 코드 삽입

--- src/components/newactivitytool/canvas.tsx
+++ src/components/newactivitytool/canvas.tsx
@@ -1,8 +1,8 @@
 import { fabric } from "fabric";
-import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { nodeActions } from "../../store/common/nodeSlice";
 import { deleteProps } from "./setting/deleteButton";
+import { useEffect, useRef, useState } from "react";
 
 export default function Canvas() {
   const dispatch = useDispatch();
@@ -22,25 +22,84 @@ export default function Canvas() {
     ...deleteProps,
     y: -1,
   });
+
   //오브젝트 기본세팅 끝
 
   useEffect(() => {
+    //id대신 canvasRef.current가 들어가도 되는군요
+    const test = new fabric.Canvas("canvas", {
+      height: window.innerWidth,
+      width: window.innerWidth,
+      preserveObjectStacking: true,
+    });
+
+    dispatch(nodeActions.setCanvas(test));
+
+    const btn = document.createElement("div");
+    btn.innerText = "test";
+    btn.style.width = 100 + "px";
+    btn.style.height = 100 + "px";
+
+    const positionBtn = (left: number, top: number) => {
+      btn.style.left = left + "px";
+      btn.style.top = top + "px";
+    };
+
+    fabric.Image.fromURL("./pencil.png", (img: any) => {
+      img.on("moving", (e: any) => {
+        positionBtn(e.target.left, e.target.top);
+      });
+      img.on("scaling", (e: any) => {
+        positionBtn(e.target.left, e.target.top);
+      });
+      console.log(img);
+      test.add(img.set({ left: 250, top: 250, angle: 30 }).scale(0.25));
+      test.renderAll();
+    });
+  }, []);
+
+  //받은 코드 시작
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    let script = document.querySelector(`script[src="./fabric.min.js"]`);
+
+    if (!script) {
+      const test = document.createElement("script");
+      test.src = "/fabric.min.js";
+      test.async = true;
+      document.body.appendChild(test);
+      const handleLoad = () => setLoading(false);
+      test.addEventListener("load", handleLoad);
+      return () => {
+        test.removeEventListener("load", handleLoad);
+      };
+    } else {
+      const handleLoad = () => setLoading(false);
+      script.addEventListener("load", handleLoad);
+      return () => {
+        script?.removeEventListener("load", handleLoad);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (loading) return;
     dispatch(
       nodeActions.setCanvas(
-        new fabric.Canvas("canvas", {
+        new fabric.Canvas(canvasRef.current, {
           height: window.innerWidth,
           width: window.innerWidth,
           preserveObjectStacking: true,
         })
       )
     );
-  }, []);
-
-  const canvas = useSelector((state: any) => state.nodeReducer.canvas);
+  }, [loading]);
+  //받은 코드 끝
 
   return (
     <>
-      <canvas id="canvas" ref={canvasRef}></canvas>
+      <canvas ref={canvasRef}></canvas>
     </>
   );
 }
