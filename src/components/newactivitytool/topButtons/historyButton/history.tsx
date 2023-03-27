9eec3927e822670b9effeddc8b8c6bfdd53fb39a
mimi <62527898+HM970506@users.noreply.github.com>
2023-03-27 13:17:58 +0900

    히스토리 완성
    스티커 부분 검증 완료

--- src/components/newactivitytool/topButtons/historyButton/history.tsx
+++ src/components/newactivitytool/topButtons/historyButton/history.tsx
@@ -10,55 +10,64 @@ export default function CanvasHistory() {
 
   const dispatch = useDispatch();
 
+  const canvasAddHandler = () => {
+    canvas.on("object:added", function (this: any, e: any) {
+      if (e.target.id == "cursor") return;
+      e.target.index = canvas.getObjects().length - 1;
+      dispatch(
+        historyActions.push({
+          act: "add",
+          value: JSON.stringify(e.target), //당시의 value. target에서는 해당 값이 계속 바뀜.
+          target: e.target,
+        })
+      );
+    });
+  };
+
+  const canvasRemoveHandler = () => {
+    canvas.on("object:removed", function (e: any) {
+      if (e.target.id == "cursor") return;
+      console.log("제거");
+      dispatch(
+        historyActions.push({
+          act: "delete",
+          value: JSON.stringify(e.target),
+          target: e.target,
+        })
+      );
+    });
+  };
+
   useEffect(() => {
     if (canvas) {
-      canvas.on("object:added", (e: any) => {
-        if (e.target.id == "cursor") return;
-        e.target.index = canvas.getObjects().length - 1;
-        dispatch(
-          historyActions.push({
-            act: "add",
-            value: JSON.stringify(e.target), //당시의 value. target에서는 해당 값이 계속 바뀜.
-            target: e.target,
-          })
-        );
-      });
-      canvas.on("before:transform", (e: any, transform: any) => {
-        console.log(transform.target);
-        if (e.target.id == "cursor") return;
-        //이동 전 위치를 저장해야 하는데 음..
-        //beforex, beforey를 줍시다.
-        console.log("수정");
-        dispatch(
-          historyActions.push({
-            act: "modify",
-            value: JSON.stringify(e.target),
-            target: e.target,
-          })
-        );
-      });
-      canvas.on("object:removed", (e: any) => {
-        if (e.target.id == "cursor") return;
-        console.log("제거");
-        dispatch(
-          historyActions.push({
-            act: "delete",
-            value: JSON.stringify(e.target),
-            target: e.target,
-          })
-        );
-        // dispatch(historyActions.push(JSON.stringify(canvas.getObjects())));
-      });
+      canvasAddHandler();
+      canvasRemoveHandler();
+      canvas.on(
+        "before:transform",
+        ({ e, transform }: { e: any; transform: any }) => {
+          if (transform.target.id == "cursor") return;
+          console.log("수정");
+          dispatch(
+            historyActions.push({
+              act: "modify",
+              value: JSON.stringify(transform.target),
+              target: transform.target,
+            })
+          );
+        }
+      );
     }
   }, [canvas]);
 
   useEffect(() => {
     console.log(nowIndex, history);
-  }, [history]);
+  }, [nowIndex]);
 
   useEffect(() => {
-    if (canvas && nowIndex >= 0) {
-      console.log(timeslip, nowIndex, history[nowIndex]);
+    if (canvas && nowIndex >= 0 && timeslip != "") {
+      canvas.__eventListeners["object:added"] = [];
+      canvas.__eventListeners["object:removed"] = [];
+
       const { act, target, value } = history[nowIndex];
 
       const changeObject = JSON.parse(value);
@@ -67,7 +76,6 @@ export default function CanvasHistory() {
         console.log("이동이나 변화");
         canvas._objects.forEach((obj: any) => {
           if (target.id == obj.id) {
-            console.log(JSON.parse(JSON.stringify(obj)), changeObject);
             //그냥 곧바로 저장하는 방법은 없나..?
             obj.top = changeObject.top;
             obj.left = changeObject.left;
@@ -80,12 +88,12 @@ export default function CanvasHistory() {
           }
         });
       } else {
-        console.log("삭제나 생성 되돌리기");
         let check = true;
 
         //있음-삭제
         canvas._objects.forEach((obj: any) => {
           if (target.id == obj.id) {
+            console.log("있으니까 삭제");
             canvas.remove(obj);
             check = false;
           }
@@ -93,6 +101,7 @@ export default function CanvasHistory() {
 
         //없음-생성
         if (check) {
+          console.log("없으니까 생성");
           const nowObjects = canvas.getObjects();
           canvas._objects = [
             ...nowObjects.slice(0, target.index),
@@ -104,19 +113,26 @@ export default function CanvasHistory() {
         //remove하면 타겟이 아예 사라지나? 테스트해 봅시다.
       }
       canvas.discardActiveObject().renderAll();
+
+      if (timeslip == "undo" && nowIndex >= 0)
+        dispatch(historyActions.setIndex(nowIndex - 1));
+      setTimeslip("");
+      canvasAddHandler();
+      canvasRemoveHandler();
     }
   }, [timeslip]);
 
   const Undo = () => {
-    if (nowIndex - 1 >= -1) {
-      // dispatch(historyActions.setIndex(nowIndex - 1));
+    if (nowIndex >= 0) {
+      console.log("undo", nowIndex);
       setTimeslip("undo");
     }
   };
 
   const Redo = () => {
     if (nowIndex + 1 < history.length) {
-      // dispatch(historyActions.setIndex(nowIndex + 1));
+      console.log("redo", nowIndex);
+      dispatch(historyActions.setIndex(nowIndex + 1));
       setTimeslip("redo");
     }
   };
@@ -127,7 +143,7 @@ export default function CanvasHistory() {
         undo
       </button>
       <button
-        disabled={nowIndex < history.length ? false : true}
+        disabled={nowIndex < history.length - 1 ? false : true}
         onClick={Redo}
       >
         redo
