16d584d2be7a235b0963929ebde73073177a321e
mimi <62527898+HM970506@users.noreply.github.com>
2023-04-14 09:51:38 +0900

    저장 버튼 리팩토링

--- src/components/newactivitytool/index.tsx
+++ src/components/newactivitytool/index.tsx
@@ -1,39 +1,23 @@
 import { useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import {
   Background,
   Overlay,
   LoadButton,
   MainButton,
   NewButton,
-} from "./style";
+} from "./styles/indexStyle";
 import SideButtons from "./sideButtons";
 import Canvas from "./canvas/canvas";
 import TopButtons from "./topButtons";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-  useQueryClient,
-} from "react-query";
-import { getFirestoreData, getStorageDataAll } from "../firestore/getData";
+import { getFirestoreData } from "../firestore/getData";
 import { ReactQueryDevtools } from "react-query/devtools";
-
-interface DATA {
-  data: string;
-  record: null | string;
-  timestampe: Date;
-}
 
 export default function NewActivityTool() {
   const newActivityTool = useRef<HTMLDialogElement>(null);
+  const canvas = useSelector((state: any) => state.nodeReducer.canvas);
   const [subButtonVisible, setSubButtonVisible] = useState<boolean>(false);
   const [activitytools, setActivitytools] = useState<boolean>(false);
-  const dispatch = useDispatch();
-
-  //할일.버튼 메모이제이션
-
-  //버튼 관련 부분 시작------------------------------------
 
   useEffect(() => {
     if (activitytools) newActivityTool.current?.showModal();
@@ -56,11 +40,6 @@ export default function NewActivityTool() {
     setActivitytools(false);
   };
 
-  //버튼 관련 부분 끝-------------------------------------
-
-  //저장 관련 부분 시작--------------------------------------
-  const canvas = useSelector((state: any) => state.nodeReducer.canvas);
-
   const getCanvas = async () => {
     const href = window.location.href.replaceAll("/", "_");
     const data = await getFirestoreData("saveData", href);
@@ -70,7 +49,10 @@ export default function NewActivityTool() {
     } else alert("저장된 데이터가 없습니다");
   };
 
-  //저장 관련 부분 끝------------------------------------------
+  const setCanvas = async () => {
+    canvas.clear();
+    activitytoolsStart();
+  };
 
   return (
     <>
@@ -87,21 +69,8 @@ export default function NewActivityTool() {
           <MainButton onClick={mainClick}>활동툴</MainButton>
           {subButtonVisible && (
             <>
-              <LoadButton
-                onClick={() => {
-                  getCanvas();
-                }}
-              >
-                불러오기
-              </LoadButton>
-              <NewButton
-                onClick={() => {
-                  canvas.clear();
-                  activitytoolsStart();
-                }}
-              >
-                새로하기
-              </NewButton>
+              <LoadButton onClick={getCanvas}>불러오기</LoadButton>
+              <NewButton onClick={setCanvas}>새로하기</NewButton>
             </>
           )}
         </>
