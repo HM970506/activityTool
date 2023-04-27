681d79b1527df19374c129b41589ba5b01fcdf2f
mimi <62527898+HM970506@users.noreply.github.com>
2023-04-27 11:12:19 +0900

    소수점 단위 자르기 타입 추가

--- src/components/newactivitytool/canvas/canvasSetting.tsx
+++ src/components/newactivitytool/canvas/canvasSetting.tsx
@@ -1,5 +1,6 @@
 //import ReactTouchEvents from "react-touch-events";
 
+import { IEvent } from "fabric/fabric-impl";
 import { canvasType } from "../types";
 
 const DeselctMultipleObjects = (canvas: canvasType) => {
@@ -13,16 +14,20 @@ export default function canvasSetting(canvas: canvasType) {
   canvas.on({
     "selection:created": () => DeselctMultipleObjects(canvas),
     "selection:updated": () => DeselctMultipleObjects(canvas),
-    "object:modified": (e: any) => {
+    "object:modified": (e: IEvent) => {
       const now = e.target;
-
-      now.width = Math.round(now.width);
-      now.height = Math.round(now.height);
-      now.top = Math.round(now.top);
-      now.left = Math.round(now.left);
-      now.angle = Math.round(now.angle);
-      now.scaleX = Math.round(now.scaleX * 1000) / 1000;
-      now.scaleY = Math.round(now.scaleY * 1000) / 1000;
+      if (now) {
+        now.width = now.width !== undefined ? Math.round(now.width) : 0;
+        now.height = now.height !== undefined ? Math.round(now.height) : 0;
+        now.top = now.top !== undefined ? Math.round(now.top) : 0;
+        now.left = now.left !== undefined ? Math.round(now.left) : 0;
+        now.angle = now.angle !== undefined ? Math.round(now.angle) : 0;
+        now.scaleX =
+          now.scaleX !== undefined ? Math.round(now.scaleX * 1000) / 1000 : 0;
+        now.scaleY =
+          now.scaleY !== undefined ? Math.round(now.scaleY * 1000) / 1000 : 0;
+      }
+      console.log(now);
     },
   });
 
