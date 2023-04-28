e8ff5a683c78217cfd297a3e156693b2b55042d7
mimi <62527898+HM970506@users.noreply.github.com>
2023-04-28 13:57:41 +0900

    혹시 모르니까 일단 노드서버 꺼놓기

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const multer = require("multer");
-const app = express();
+// const express = require("express");
+// const multer = require("multer");
+// const app = express();
 
-// Set up multer middleware to handle file uploads
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-    s,
-  }),
-});
+// // Set up multer middleware to handle file uploads
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "uploads/");
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname);
+//     },
+//     s,
+//   }),
+// });
 
-// Set up route to handle audio file uploads
-app.post("/upload-audio", upload.single("audio"), (req, res) => {
-  console.log(req.file);
-  res.json({ message: "File uploaded successfully" });
-});
+// // Set up route to handle audio file uploads
+// app.post("/upload-audio", upload.single("audio"), (req, res) => {
+//   console.log(req.file);
+//   res.json({ message: "File uploaded successfully" });
+// });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+// app.listen(8080, () => {
+//   console.log("Server is running on port 8080");
+// });
