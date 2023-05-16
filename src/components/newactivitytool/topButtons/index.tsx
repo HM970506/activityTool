e0fcb5cd6410bd2776f094335c7456f20b06b0f1
mimi <62527898+HM970506@users.noreply.github.com>
2023-05-16 14:22:16 +0900

    핀치줌 테스트2

--- src/components/newactivitytool/topButtons/index.tsx
+++ src/components/newactivitytool/topButtons/index.tsx
@@ -3,10 +3,17 @@ import CanvasHistory from "./historyButton";
 
 import { TopArea, TopButtonContainer } from "./style";
 import { useState } from "react";
+import { ReducersType } from "../types";
+import { useSelector } from "react-redux";
 
 export default function TopButtons() {
   const [zoom, setZoom] = useState<any>(null);
-  const bind = usePinch((state) => setZoom(state));
+  const canvas = useSelector((state: ReducersType) => state.nodeReducer.canvas);
+  const bind = usePinch(({ da }) => {
+    const [scaleValue] = da;
+    setZoom(scaleValue);
+    // canvas.zoomToPoint(point, zoom);
+  });
 
   return (
     <TopArea>
