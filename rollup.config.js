b4e78d6950874d3eaacedcc1d91418ded68bf1d5
mimi <62527898+HM970506@users.noreply.github.com>
2023-07-05 14:33:51 +0900

    라이브러리화 파일 준비

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import svgr from "@svgr/rollup";
+import image from "@rollup/plugin-image";
+import url from "@rollup/plugin-url";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
+import jsx from "acorn-jsx";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js", // Output file
+        format: "es",
+      },
+    ],
+    external: ["react", "react-dom", "styled-components"],
+    plugins: [
+      resolve(),
+      commonjs({ include: "node_modules/**" }), //node_modules에서 모듈을 불러올수 있도록 해줌, ts/tsx 파일도 불러올수 있음
+      url(), //미디어 파일을 dataURI 형태로 불러와서 사용 할 수 있게 해줌
+      svgr(), //svg를 컴포넌트로 사용 할 수 있게 해줌
+      peerDepsExternal(), //peerDependencies에 설치된 라이브러리들을 external모듈로 설정하여 번들 결과물에서 제외
+      typescript({ tsconfig: "./tsconfig.json" }), //typescript 플러그인
+      image(),
+      json({ compact: true }),
+      babel({
+        babelHelpers: "bundled",
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        exclude: "node_modules/**",
+        include: ["src/**/*"],
+      }),
+    ],
+  },
+];
--- src/team.tsx
+++ src/team.tsx
@@ -1,20 +1,36 @@
-import Background from "./background.png";
 import styled from "styled-components";
 import NewActivityTool from "./components/newactivitytool";
+import { useState } from "react";
+import { Reset } from "styled-reset";
 
 const TestImage = styled.img`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  z-index: -1;
-  top: 0;
-  left: 0;
+  width: auto;
+  height: 100vh;
+  display: relative;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
 `;
 
 export default function Team() {
+  const [background, setBackground] = useState<string>("");
+  //@ts-ignore
+  window.backgroundFlutterURL = (data: string) => {
+    if (data) {
+      console.log("받았어욧");
+      setBackground(data);
+    } else console.log("배경 데이터를 받아오지 못했습니다");
+  };
   return (
     <>
-      <TestImage src={Background} />
+      <Reset />
+      <Container>
+        <TestImage src={background} />
+      </Container>
       <NewActivityTool />
     </>
   );
